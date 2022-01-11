74ec9cfe8376015bde73b8b632cd7e4435ffeb37
paulow540 <halleluyahpaul@gmail.com>
2022-01-11 18:57:04 +0100

    i added something to the function of the view

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,19 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(max_length=120, widget=forms.TextInput(
+                                    attrs={
+                                        "placeholder":"Your title",   
+                                    }
+                                )) 
+    description = forms.CharField(
+                    required=False, 
+                    widget=forms.Textarea(
+                                    attrs={
+                                        "class":"new-class-name two",   
+                                    }
+                                )) 
+    price = forms.DecimalField(initial =199.9, decimal_places=2, max_digits=500) 
     class Meta:
         model = Product
         fields = [
@@ -12,6 +25,16 @@ class ProductForm(forms.ModelForm):
         ]
 
 class RawProductForm(forms.Form):
-    title = forms.CharField(max_length=120) 
-    description = forms.CharField() 
-    price = forms.DecimalField(decimal_places=2, max_digits=500) 
+    title = forms.CharField(max_length=120, widget=forms.TextInput(
+                                    attrs={
+                                        "placeholder":"Your title",   
+                                    }
+                                )) 
+    description = forms.CharField(
+                    required=False, 
+                    widget=forms.Textarea(
+                                    attrs={
+                                        "class":"new-class-name two",   
+                                    }
+                                )) 
+    price = forms.DecimalField(initial =199.9, decimal_places=2, max_digits=500) 
--- products/views.py
+++ products/views.py
@@ -3,23 +3,32 @@ from .models import Product
 from .forms import ProductForm,RawProductForm
 
 # Create your views here.
+# def product_create_view(request):
+#     my_form =RawProductForm()
+#     if request.method =="POST":
+#         my_form = RawProductForm(request.POST)
+#         print(my_form)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context ={
+#         "form":my_form
+#        }
+    
+#     return render(request, "products/product_create.html", context)
+
 def product_create_view(request):
-    my_form =RawProductForm()
-    if request.method =="POST":
-        my_form = RawProductForm(request.POST)
-        print(my_form)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+    form =ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=ProductForm()
     context ={
-        "form":my_form
+        "form":form
        }
     
     return render(request, "products/product_create.html", context)
-
-
 
 
 
